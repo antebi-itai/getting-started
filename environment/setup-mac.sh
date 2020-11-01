f36295fd1aff3caf49ba512535a6ddea1575cb0b
Ben Feinstein <feinsteinben@gmail.com>
2020-11-01 14:07:48 +0200

    update environment

--- environment/setup-mac.sh
+++ environment/setup-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -i
 cd "$(dirname "$0")"
-conda env create -f wis19-cpu.yml
+conda env create -f wis-cv.yml
 read -rsp $'Press any key to continue...\n' -n 1 key
--- environment/setup-win.bat
+++ environment/setup-win.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
-call conda env create -f wis19-cpu.yml
+call conda env create -f wis-cv.yml
 pause
 exit
